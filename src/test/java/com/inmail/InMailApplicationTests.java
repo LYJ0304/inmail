01a35e4f9f167129d01df5388e19e5ce81f5befe
LYJ0304 <omlmg0304@naver.com>
2025-08-31 17:38:57 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/inmail/InMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
