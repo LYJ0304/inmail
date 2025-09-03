6cabd9091e8a6ac5a9a2e425b80fae35cd75f342
LYJ0304 <omlmg0304@naver.com>
2025-09-03 09:55:13 +0900

    feat: SecurityConfig를 통한 보안설정 추가

--- src/main/java/com/inmail/config/SecurityConfig.java
+++ src/main/java/com/inmail/config/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.inmail.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // 스프링 컨테이너에 의해 빈의 정의하고 관리하는데 사용됨
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->auth
+                        .requestMatchers("/api/auth/signup", "h2/console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        http.headers(headers->headers.frameOptions(frame->frame.disable()));
+
+        return http.build();
+    }
 }
