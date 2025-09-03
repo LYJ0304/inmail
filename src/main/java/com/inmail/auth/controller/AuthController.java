0f7b7c240d988b9d89ea0168bfd123be2e4b5b04
LYJ0304 <omlmg0304@naver.com>
2025-09-03 10:12:14 +0900

    feat: 회원가입 관련 controller 수정

--- src/main/java/com/inmail/auth/controller/AuthController.java
+++ src/main/java/com/inmail/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.inmail.auth.dto.UserSummary;
 import com.inmail.auth.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @PostMapping("/signup")
     public ResponseEntity<ApiResponse<UserSummary>> signup(@Valid @RequestBody SignupRequest req) {
         UserSummary result = authService.signup(req);
         return ResponseEntity.ok(ApiResponse.ok(result));
